a620ca244cdc00079ab7d0c144bff9bffd213145
GILET Ronan <ronan.gilet@gmail.com>
2026-07-01 15:13:39 +0200

    Livre @Entity

--- biblio-boot/src/main/java/biblio_boot/model/Livre.java
+++ biblio-boot/src/main/java/biblio_boot/model/Livre.java
@@ -2,6 +2,7 @@ package biblio_boot.model;
 
 import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
