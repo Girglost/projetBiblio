9e1e3999cef9b1136b570654275bf6ddd5dda51e
YohannLG01 <yohannleguen@hotmail.com>
2026-07-01 15:51:51 +0200

    SecurityConfig de base permit all

--- biblio-boot/src/main/java/biblio_boot/security/SecurityConfig.java
+++ biblio-boot/src/main/java/biblio_boot/security/SecurityConfig.java
@@ -1,5 +1,25 @@
 package biblio_boot.security;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true) // Activer les annotations @PreAuthorize & @PostAuthorize
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Configuration des autorisations
+        http.authorizeHttpRequests(auth -> {
+            // On commence toujours par le plus spécifique, pour terminer par le plus
+            // général
+
+            // Les utilisateurs doivent être authentifiés pour accéder à /quelquechose
+            auth.requestMatchers("/**").permitAll();
+        });
+
+        return http.build();
+    }
 }
