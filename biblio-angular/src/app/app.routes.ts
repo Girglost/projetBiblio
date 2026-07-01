1e95316324ac7f1a33e9e65439d1174bd86b71da
GILET Ronan <ronan.gilet@gmail.com>
2026-07-01 16:30:38 +0200

    Navigation faite

--- biblio-angular/src/app/app.routes.ts
+++ biblio-angular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"editeur", component:Editeur},
+  {path:"livre", component:Livre},
+  {path:"auteur", component:Auteur},
+  {path:"collection", component:Collection}
+];
