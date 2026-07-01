10f568014e2f4b7a3238501f43873308ced6b461
GILET Ronan <ronan.gilet@gmail.com>
2026-07-01 16:37:52 +0200

    App.routes
    Corrections imports + component name

--- biblio-angular/src/app/app.routes.ts
+++ biblio-angular/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
+import { EditeurPage } from './page/editeur-page/editeur-page';
+import { CollectionPage } from './page/collection-page/collection-page';
+import { LivrePage } from './page/livre-page/livre-page';
+import { AuteurPage } from './page/auteur-page/auteur-page';
+
 
 export const routes: Routes = [
-  {path:"editeur", component:Editeur},
-  {path:"livre", component:Livre},
-  {path:"auteur", component:Auteur},
-  {path:"collection", component:Collection}
+  {path:"editeur", component:EditeurPage},
+  {path:"livre", component:LivrePage},
+  {path:"auteur", component:AuteurPage},
+  {path:"collection", component:CollectionPage}
 ];
