ae4c788b6cbc31b688887f35e2748fa9cf133291
GILET Ronan <ronan.gilet@gmail.com>
2026-07-01 15:29:49 +0200

    Debut test

--- biblio-boot/src/test/java/biblio_boot/dao/LivreDaoTest.java
+++ biblio-boot/src/test/java/biblio_boot/dao/LivreDaoTest.java
@@ -1,5 +1,14 @@
 package biblio_boot.dao;
 
-public class LivreDaoTest {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import biblio_boot.service.LivreService;
+
+@DataJpaTest
+public class LivreDaoTest {
+    @Autowired
+    private LivreService livreSrv;
+
+    
 }
