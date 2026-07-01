5c5cc57c942477a857ffba02ccacc0e9b01a848e
YohannLG01 <yohannleguen@hotmail.com>
2026-07-01 15:30:23 +0200

    Correction import Editeur

--- biblio-boot/src/main/java/biblio_boot/model/Editeur.java
+++ biblio-boot/src/main/java/biblio_boot/model/Editeur.java
@@ -1,11 +1,10 @@
 package biblio_boot.model;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Editeur {
