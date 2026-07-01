af434f82e5e88b42403f3bbd76eb49671cad32a6
eimera <marie.andre_a@yahoo.com>
2026-07-01 14:45:57 +0200

    Classe auteur

--- biblio-boot/src/main/java/biblio_boot/model/Auteur.java
+++ biblio-boot/src/main/java/biblio_boot/model/Auteur.java
@@ -1,12 +1,51 @@
 package biblio_boot.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Auteur {
 
-    private string nom;
-    private string prenom;
-    private string nationalite;
-
+    private String nom;
+    private String prenom;
+    private String nationalite;
+	
     
+    //getter setter
+    
+    public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getNationalite() {
+		return nationalite;
+	}
+	public void setNationalite(String nationalite) {
+		this.nationalite = nationalite;
+	}
+	
+	//constructeur
+	
+	public Auteur(String nom, String prenom, String nationalite) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.nationalite = nationalite;
+	}
+
+	//constructeur vide
+	
+	public Auteur() {}
+	
+	
+    
+	
 
 
 }
