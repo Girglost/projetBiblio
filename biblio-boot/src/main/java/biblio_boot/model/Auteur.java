7139d56b0812561c751f52dad01a007f5ef819ec
YohannLG01 <yohannleguen@hotmail.com>
2026-07-01 15:57:25 +0200

    Correction getId Auteur

--- biblio-boot/src/main/java/biblio_boot/model/Auteur.java
+++ biblio-boot/src/main/java/biblio_boot/model/Auteur.java
@@ -8,55 +8,58 @@ import jakarta.persistence.Id;
 @Entity
 public class Auteur {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-    private String nom;
-    private String prenom;
-    private String nationalite;
-	
-    
-    //getter setter
-    public Integer getIdInteger() {
+	private String nom;
+	private String prenom;
+	private String nationalite;
+
+	// getter setter
+	public Integer getId() {
 		return id;
 	}
-    public void setId(Integer id) {
+
+	public void setId(Integer id) {
 		this.id = id;
-    }
-    public String getNom() {
+	}
+
+	public String getNom() {
 		return nom;
 	}
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
 	public String getPrenom() {
 		return prenom;
 	}
+
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
+
 	public String getNationalite() {
 		return nationalite;
 	}
+
 	public void setNationalite(String nationalite) {
 		this.nationalite = nationalite;
 	}
-	
-	//constructeur
-	
+
+	// constructeur
+
 	public Auteur(String nom, String prenom, String nationalite) {
 		this.nom = nom;
 		this.prenom = prenom;
 		this.nationalite = nationalite;
 	}
 
-	//constructeur vide
-	
-	public Auteur() {}
-	
-    
-	
+	// constructeur vide
 
+	public Auteur() {
+	}
 
 }
