452f3dff5cb2410404e4aae6466af78201daf491
GILET Ronan <ronan.gilet@gmail.com>
2026-07-01 15:01:11 +0200

    Livre+dao

--- biblio-boot/src/main/java/biblio_boot/dao/LivreDAO.java
+++ biblio-boot/src/main/java/biblio_boot/dao/LivreDAO.java
@@ -1,5 +1,17 @@
 package biblio_boot.dao;
 
-public class LivreDAO {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import biblio_boot.model.Livre;
+
+public interface LivreDAO extends JpaRepository<Livre,Integer>{
+    /* 
+    public List<Livre> findByAnneeBetween(int a, int b);
+    public List<Livre> findByAnneeLesserThan(int a);
+    public List<Livre> findByAnneeGreaterThan(int a);
+    public List<Livre> findByTitreLike(String s);
+    public Livre findByTitre(String s);
+    */
 }
