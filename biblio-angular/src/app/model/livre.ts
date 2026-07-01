51a70a3973376be2ac314b416487f4aa43e0c222
GILET Ronan <ronan.gilet@gmail.com>
2026-07-01 17:30:02 +0200

    Livre service + model fait

--- biblio-angular/src/app/model/livre.ts
+++ biblio-angular/src/app/model/livre.ts
@@ -1,1 +1,13 @@
-export interface Livre {}
+import { Auteur } from "./auteur";
+import { Collection } from "./collection";
+import { Editeur } from "./editeur";
+
+export interface Livre {
+  id:number,
+  titre:string,
+  resumer:string,
+  annee:number,
+  auteur:Auteur,
+  editeur:Editeur,
+  collection:Collection,
+}
