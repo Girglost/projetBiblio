f2f4ac4e711c2fe3f90418eaca035572bc5a9ade
YohannLG01 <yohannleguen@hotmail.com>
2026-07-02 11:59:21 +0200

    ROUTE LOGIN

--- biblio-angular/src/app/app.routes.ts
+++ biblio-angular/src/app/app.routes.ts
@@ -1,19 +1,21 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './guard/auth-guard';
 import { AuteurPage } from './page/auteur-page/auteur-page';
+import { AvisPage } from './page/avis-page/avis-page';
 import { CollectionPage } from './page/collection-page/collection-page';
 import { EditeurPage } from './page/editeur-page/editeur-page';
 import { LivrePage } from './page/livre-page/livre-page';
+import { LoginPage } from './page/login-page/login-page';
 import { UtilisateurPage } from './page/utilisateur-page/utilisateur-page';
-import { AvisPage } from './page/avis-page/avis-page';
 
 
 export const routes: Routes = [
-  { path: "editeur", component: EditeurPage },
-  { path: "livre", component: LivrePage },
-  { path: "auteur", component: AuteurPage },
-  { path: "collection", component: CollectionPage },
-  { path: "utilisateur", component: UtilisateurPage},
-  { path: "avis", component: AvisPage},
+  { path: "editeur", component: EditeurPage, canActivate: [authGuard] },
+  { path: "livre", component: LivrePage, canActivate: [authGuard] },
+  { path: "auteur", component: AuteurPage, canActivate: [authGuard] },
+  { path: "collection", component: CollectionPage, canActivate: [authGuard] },
+  { path: "utilisateur", component: UtilisateurPage, canActivate: [authGuard] },
+  { path: "avis", component: AvisPage, canActivate: [authGuard] },
+  { path: "login", component: LoginPage, canActivate: [authGuard] }
 
 ];
