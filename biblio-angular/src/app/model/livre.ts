381d39976df746bd04804c0473e63cf6abb51896
GILET Ronan <ronan.gilet@gmail.com>
2026-07-02 12:55:44 +0200

    Angular
    LivrePage

--- biblio-angular/src/app/model/livre.ts
+++ biblio-angular/src/app/model/livre.ts
@@ -3,11 +3,11 @@ import { Collection } from "./collection";
 import { Editeur } from "./editeur";
 
 export interface Livre {
-  id:number,
+  id?:number,
   titre:string,
-  resumer:string,
+  resumer?:string,
   annee:number,
   auteur:Auteur,
   editeur:Editeur,
-  collection:Collection,
+  collection?:Collection,
 }
