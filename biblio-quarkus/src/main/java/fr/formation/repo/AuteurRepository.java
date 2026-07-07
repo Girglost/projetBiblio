aaddbb5e33ebbc729338f7dc3efc6def45017447
Hestia <clea.vcd@gmail.com>
2026-07-07 12:24:37 +0200

    app scoped auteur

--- biblio-quarkus/src/main/java/fr/formation/repo/AuteurRepository.java
+++ biblio-quarkus/src/main/java/fr/formation/repo/AuteurRepository.java
@@ -2,7 +2,9 @@ package fr.formation.repo;
 
 import fr.formation.model.Auteur;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class AuteurRepository implements PanacheRepositoryBase<Auteur,Integer> {
 
 
